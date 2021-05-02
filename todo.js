7dc4b4a5801579ea4414c476478a90fb7e4ce680
kimjeehey <80799002+kimjeehey@users.noreply.github.com>
2021-05-02 18:13:15 +1000

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,21 +4,15 @@ const toDoForm = document.querySelector(".js-toDoForm"), toDoInput = toDoForm.qu
     
 
 const TODO_LS = "toDos";
-const DONE_LS = "toDoDone";
 
-function filterFn(toDo) {
-    return toDo.id === 1;
-}
-
-function filterFn(toDoDone) {
-    return toDoDone.id === 1;
-}
+// function filterFn(toDo) {
+//     return toDo.id === 1;
+// }
 
 let toDos = [];
 
 function saveToDo(){
     localStorage.setItem(TODO_LS, JSON.stringify(toDos));
-    localStorage.setItem(DONE_LS, JSON.stringify(toDoDone));
 }
 
 function deleteToDo(event) {
@@ -34,7 +28,7 @@ function deleteToDo(event) {
     saveToDo();
 }
 
-    function paintToDo(text) {
+function paintToDo(text) {
     const li = document.createElement("li");
     const doneBtn = document.createElement("input");
     const delBtn = document.createElement("button");
@@ -42,27 +36,16 @@ function deleteToDo(event) {
     const newId = toDos.length + 1;
     
     doneBtn.setAttribute("type", "checkbox");
-    // doneBtn.innerText = "Done";
     delBtn.innerText = "𐄂";
     delBtn.addEventListener("click", deleteToDo)
     span.innerText = text;
 
-    toDoList.appendChild(li);
+    toDoList.append(li);
     li.appendChild(doneBtn);
     li.appendChild(span);
     li.appendChild(delBtn);
     
     
-    function handleDoneClick() {
-        if (doneBtn.checked === true) {
-            span.style.setProperty('text-decoration', 'line-through');
-            toDoList.appendChild(li);
-
-        } else {
-            span.style.removeProperty('text-decoration', 'line-through');
-        }
-    }   
-    doneBtn.addEventListener("click", handleDoneClick);
 
     li.id = newId;
     const toDoObj = {
@@ -71,18 +54,15 @@ function deleteToDo(event) {
     };
     toDos.push(toDoObj);
     saveToDo();
-}
 
-function doneToDo(text) {
-    li.id = newIdDone;
+    function handleDoneClick() {
+        if (doneBtn.checked === true) {
+            span.classList.add('checked');
+            toDoList.appendChild(li);
+        }
+    }
 
-    const toDoDoneObj = {
-        text: text,
-        id: newIdDone
-    };
-
-    toDoDone.push(toDoDoneObj);
-    saveToDoDone();
+    doneBtn.addEventListener("click", handleDoneClick);   
 }
 
 function handleSubmit(event) {
@@ -93,18 +73,13 @@ function handleSubmit(event) {
 }
 
 function loadToDo(){
-    const loadToDo = localStorage.getItem(TODO_LS), moveToDo = localStorage.getItem(DONE_LS);
+    const loadToDo = localStorage.getItem(TODO_LS)
     if(loadToDo !== null) {
         const parsedToDos = JSON.parse(loadToDo);
         parsedToDos.forEach(function(toDo) {
             paintToDo(toDo.text);
         });
-    } else if (moveToDo !== null) {
-        const parsedToDoDones = JSON.parse(moveToDo);
-        parsedToDoDones.forEach(function(toDoDone) {
-            doneToDo(toDoDone.text);
-        });
-    }
+    }   
 }
 
 function init() {
